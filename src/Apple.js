81477e642fa27ec17e81b20896da1c893432e2ff
Jeel <jeelmaheshwari812@gmail.com>
2023-05-29 07:46:39 +0530

    Added useState, Textfield, onChange element and Avatar using material UI.

--- src/Apple.js
+++ src/Apple.js
@@ -3,13 +3,17 @@ import { useNavigate } from "react-router-dom";
 
 import Button from '@mui/material/Button';
 import { useState } from "react";
+import TextField from '@mui/material/TextField';
+import { Avatar } from '@mui/material'; 
+import { blue } from "@mui/material/colors";
 
 export const Apple = () => {
     const [name, setName] = useState("");
-    const [email, setEmail] = useState("");   
+    const [email, setEmail] = useState("");
     const Navigate = useNavigate();
+    // we can provide predefined input values to input tag by placing values in curly braces.
 
-    const onHomePageButtonClick = () =>{
+    const onHomePageButtonClick = () => {
         console.log("Button clicked!");
         console.log("Name: ", name);
         console.log("Email: ", email);
@@ -18,19 +22,44 @@ export const Apple = () => {
     };
 
     return (
-        <div>
-            <div>Apple Page </div>
+        <div
+            style={{
+                padding: 5,
+            }}
+        >
+            <div style={{
+                display: "flex",
+                justifycontent: "flex-end",
+                alignitems: "center",
+                columngap: "5px",
+            }}>
+                <Avatar sx={{ bgcolor: "blue" }}>JM</Avatar>
+                <span>Jeel Maheshwari</span>
+            </div>
+            <div
+                style={{
+                    padding: 5,
+                    display: "flex",
+                    flexDirection: "column",
+                    rowGap: 8,
+                }}>
 
-            <input 
-                type="text" 
-                placeholder="Name" 
-                onChange={(e) => {
-                    setName(e.target.value);
-                }}
-            />
-            <input type="email" placeholder="email" />
-            <Button variant="contained" onClick={onHomePageButtonClick}>Navigate to Home page</Button>
-            {/* <button onClick={onHomePageButtonClick}>Navigate to Home page</button> */}
+                <TextField
+                    variant="outlined"
+                    type="text" value={name}
+                    label="Name" placeholder="Name" 
+                    onChange={(e) => setName(e.target.value)} />
+                <TextField
+                    variant="outlined"
+                    type="email" value={email}
+                    label="Email" placeholder="email" 
+                    onChange={(e) => setName(e.target.value)} />
+
+                <Button variant="contained" onClick={onHomePageButtonClick}>
+                    Submit
+                </Button>
+                {/* <button onClick={onHomePageButtonClick}>Navigate to Home page</button> */}
+            </div>
         </div>
     );
 };
